037beec74a5444c19b91bcc96124fc3f4021425e
Enrico Sponholz Barreto <enricosbarreto@gmail.com>
2026-06-10 22:49:34 -0300

    feat: add index and routes configuration for flask app

--- index.py
+++ index.py
@@ -4,5 +4,6 @@ from routes import register_routes
 app = Flask(__name__)
 register_routes(app)
 
+
 if __name__ == "__main__":
     app.run(debug=True)
--- routes.py
+++ routes.py
@@ -5,3 +5,8 @@ def register_routes(app):
     @app.route("/")
     def homepage():
         return render_template("site.html")
+    
+    @app.route("/Minha-Lista")
+    def minha_lista():
+        return "toma a lista do pae otarios"
+    
